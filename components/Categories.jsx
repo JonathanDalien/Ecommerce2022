2b3625531bb25d95e4a0ecac0524e43c4b5d0419
Jonathan Dalien <62235886+JonathanDalien@users.noreply.github.com>
2022-12-18 13:13:31 +0100

    Update Categories.jsx

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -11,7 +11,7 @@ const Categories = ({ categories }) => {
       </h1>
       <div className="flex justify-start gap-8 overflow-auto py-4 lg:gap-24 xl:justify-center">
         <div className="w-[200px]">
-          <Link href="">
+          <Link href="/offers">
             <div className="category-card flex h-[200px] w-[200px] items-center justify-center rounded-full bg-blue-800 transition-all hover:scale-105 hover:bg-red-500">
               <h1 className="text-3xl font-bold text-white">Angebote</h1>
             </div>
